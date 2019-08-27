e7a1dd60418b17829d6b0f729e0ccdca2f806142
Kevin <stradtkt22@gmail.com>
2019-08-26 23:56:45 -0400

    Created the layout

--- app/src/main/java/com/stradtman/braintrainer/MainActivity.java
+++ app/src/main/java/com/stradtman/braintrainer/MainActivity.java
@@ -3,12 +3,22 @@ package com.stradtman.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    Button buttonStart;
+
+    public void start(View view) {
+        buttonStart.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonStart = (Button) findViewById(R.id.buttonGo);
     }
 }
