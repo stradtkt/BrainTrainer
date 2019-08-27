5a22a558419dfc181d187673b571e9aca5aecf67
Kevin <stradtkt22@gmail.com>
2019-08-27 00:47:16 -0400

    Updates the score

--- app/src/main/java/com/stradtman/braintrainer/MainActivity.java
+++ app/src/main/java/com/stradtman/braintrainer/MainActivity.java
@@ -3,9 +3,11 @@ package com.stradtman.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -14,10 +16,31 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     Button buttonStart;
+    TextView resultTextView;
+    TextView pointsTextView;
     ArrayList<Integer> answers = new ArrayList<Integer>();
     int locationOfCorrectAnswer;
+    int score = 0;
+    int numberOfQuestions = 0;
+
+    public void chooseAnswer(View view) {
+        Log.d(TAG, "chooseAnswer: starts");
+        if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+//            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+            score++;
+            resultTextView.setText("Correct!");
+        } else {
+            resultTextView.setText("Wrong");
+        }
+        numberOfQuestions++;
+        pointsTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+        Log.d(TAG, "chooseAnswer: ends");
+    }
+
     public void start(View view) {
+        Log.d(TAG, "start: starts");
         buttonStart.setVisibility(View.INVISIBLE);
+        Log.d(TAG, "start: ends");
     }
 
     @Override
@@ -31,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
         Button button1 = (Button) findViewById(R.id.button1);
         Button button2 = (Button) findViewById(R.id.button2);
         Button button3 = (Button) findViewById(R.id.button3);
+        resultTextView = (TextView) findViewById(R.id.correctTextView);
+        pointsTextView = (TextView) findViewById(R.id.scoreTextView);
 
         Random random = new Random();
         int a = random.nextInt(21);
